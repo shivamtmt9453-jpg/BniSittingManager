6dfb938cb0eaf81e1e0e9b42d17a806d28b5c78b
Trianglemind PC <trianglemind@outlook.com>
2026-04-21 11:46:54 +0530

    comit by shivam

--- Areas/Admin/Controllers/DashboardController.cs
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using BniSittingManager.Areas.Admin.Models;
+using BniSittingManager.Controllers;
+using BniSittingManager.Data;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient; 
-using BniSittingManager.Controllers;
 using System.Data;
 using System.Text;
-using BniSittingManager.Data;
 
 namespace BniSittingManager.Areas.Admin.Controllers
 {
@@ -21,10 +22,27 @@ namespace BniSittingManager.Areas.Admin.Controllers
         }
 
 
-        public IActionResult Index()
-        {
-            return View();
-        }   
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {  
+            var model = new DashboardViewModel();
+
+            // Execute SP with Action parameter
+            var dtDashboard = await _dbLayer.ExecuteSPAsync(
+                "sp_GetDashboardData",
+                new[] { new SqlParameter("@Action", "GetDashboardStats") }
+            );
+
+            if (dtDashboard.Rows.Count > 0)
+            {
+                var row = dtDashboard.Rows[0];
+
+                model.TotalBniMember = row["TotalBniMember"] != DBNull.Value ? (int)row["TotalBniMember"] : 0;
+                model.TotalBniVisiter = row["TotalBniVisiter"] != DBNull.Value ? (int)row["TotalBniVisiter"] : 0;
+            }
+             
+            return View(model);
+        }
 
         public async Task<IActionResult> Logout()
         {
